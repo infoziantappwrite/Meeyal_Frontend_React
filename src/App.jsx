07897e7110f6feddd67d6cbb5862ba77816af45b
harirajhari <hariraj121203@gmailcom>
2025-03-20 11:52:46 +0530

    font vercel issuse

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter  as Router, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter  as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./component/Header/Header";
 import Mainbody from "./component/Mainbody/Mainbody";
 import "./index.css";
