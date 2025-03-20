2c4284dec946cc09bd0663ed11d9426f4f09c894
harirajhari <hariraj121203@gmailcom>
2025-03-20 11:29:10 +0530

    font vercel issuse

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    assetsInlineLimit: 0, // Ensures fonts are not inlined as Base64
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]' // Keeps assets organized
-      }
-    }
-  },
-  server: {
-    watch: {
-      usePolling: true, // Fixes issues with file watching
-    }
-  }
+  
 })
